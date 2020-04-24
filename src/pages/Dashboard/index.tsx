834c5cb1949353b0a684fa9a6c42693a0e60b0ea
leticiaenz <leticia.enz11@gmail.com>
2020-04-23 23:26:57 -0400

    Class06: Use styled components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Title } from './style';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return <Title>Explore repositories no GitHub</Title>;
 };
 
 export default Dashboard;
